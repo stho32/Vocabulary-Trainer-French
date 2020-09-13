d30fe7139a0363707772388a0b07ecc753c858c7
Stefan Hoffmann <stho32github@gmail.com>
2020-09-13 10:31:44 +0200

    more french vocabularies

--- js/alle-trainings.js
+++ js/alle-trainings.js
@@ -1,6 +1,6 @@
 "use strict";
 
-var _frenchTraining$frage, _frenchTraining$frage2, _frenchTraining$frage3, _frenchTraining$frage4, _frenchTraining$frage5, _frenchTraining$frage6, _frenchTraining$frage7, _frenchTraining$frage8, _frenchTraining$frage9, _frenchTraining$frage10;
+var _frenchTraining$frage, _frenchTraining$frage2, _frenchTraining$frage3, _frenchTraining$frage4, _frenchTraining$frage5, _frenchTraining$frage6, _frenchTraining$frage7, _frenchTraining$frage8, _frenchTraining$frage9, _frenchTraining$frage10, _frenchTraining$frage11, _frenchTraining$frage12, _frenchTraining$frage13, _frenchTraining$frage14, _frenchTraining$frage15, _frenchTraining$frage16, _frenchTraining$frage17, _frenchTraining$frage18, _frenchTraining$frage19, _frenchTraining$frage20, _frenchTraining$frage21, _frenchTraining$frage22, _frenchTraining$frage23, _frenchTraining$frage24, _frenchTraining$frage25, _frenchTraining$frage26, _frenchTraining$frage27, _frenchTraining$frage28, _frenchTraining$frage29, _frenchTraining$frage30, _frenchTraining$frage31, _frenchTraining$frage32, _frenchTraining$frage33, _frenchTraining$frage34, _frenchTraining$frage35, _frenchTraining$frage36, _frenchTraining$frage37, _frenchTraining$frage38, _frenchTraining$frage39, _frenchTraining$frage40, _frenchTraining$frage41, _frenchTraining$frage42, _frenchTraining$frage43, _frenchTraining$frage44, _frenchTraining$frage45, _frenchTraining$frage46, _frenchTraining$frage47, _frenchTraining$frage48, _frenchTraining$frage49, _frenchTraining$frage50, _frenchTraining$frage51, _frenchTraining$frage52;
 
 function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
 
@@ -16,11 +16,58 @@ var frenchTraining = {
 (_frenchTraining$frage3 = frenchTraining.fragen).push.apply(_frenchTraining$frage3, _toConsumableArray(Beidseitig("3", "trois")));
 (_frenchTraining$frage4 = frenchTraining.fragen).push.apply(_frenchTraining$frage4, _toConsumableArray(Beidseitig("4", "quatre")));
 (_frenchTraining$frage5 = frenchTraining.fragen).push.apply(_frenchTraining$frage5, _toConsumableArray(Beidseitig("5", "cinq")));
-
 (_frenchTraining$frage6 = frenchTraining.fragen).push.apply(_frenchTraining$frage6, _toConsumableArray(Beidseitig("6", "six")));
 (_frenchTraining$frage7 = frenchTraining.fragen).push.apply(_frenchTraining$frage7, _toConsumableArray(Beidseitig("7", "sept")));
 (_frenchTraining$frage8 = frenchTraining.fragen).push.apply(_frenchTraining$frage8, _toConsumableArray(Beidseitig("8", "huit")));
 (_frenchTraining$frage9 = frenchTraining.fragen).push.apply(_frenchTraining$frage9, _toConsumableArray(Beidseitig("9", "neuf")));
 (_frenchTraining$frage10 = frenchTraining.fragen).push.apply(_frenchTraining$frage10, _toConsumableArray(Beidseitig("10", "dix")));
+
+(_frenchTraining$frage11 = frenchTraining.fragen).push.apply(_frenchTraining$frage11, _toConsumableArray(Beidseitig("11", "onze")));
+(_frenchTraining$frage12 = frenchTraining.fragen).push.apply(_frenchTraining$frage12, _toConsumableArray(Beidseitig("12", "douze")));
+(_frenchTraining$frage13 = frenchTraining.fragen).push.apply(_frenchTraining$frage13, _toConsumableArray(Beidseitig("13", "treize")));
+(_frenchTraining$frage14 = frenchTraining.fragen).push.apply(_frenchTraining$frage14, _toConsumableArray(Beidseitig("14", "quatorze")));
+(_frenchTraining$frage15 = frenchTraining.fragen).push.apply(_frenchTraining$frage15, _toConsumableArray(Beidseitig("15", "quinze")));
+(_frenchTraining$frage16 = frenchTraining.fragen).push.apply(_frenchTraining$frage16, _toConsumableArray(Beidseitig("16", "seize")));
+(_frenchTraining$frage17 = frenchTraining.fragen).push.apply(_frenchTraining$frage17, _toConsumableArray(Beidseitig("17", "dix-sept")));
+(_frenchTraining$frage18 = frenchTraining.fragen).push.apply(_frenchTraining$frage18, _toConsumableArray(Beidseitig("18", "dix-huit")));
+(_frenchTraining$frage19 = frenchTraining.fragen).push.apply(_frenchTraining$frage19, _toConsumableArray(Beidseitig("19", "dix-neuf")));
+(_frenchTraining$frage20 = frenchTraining.fragen).push.apply(_frenchTraining$frage20, _toConsumableArray(Beidseitig("20", "vingt")));
+
+(_frenchTraining$frage21 = frenchTraining.fragen).push.apply(_frenchTraining$frage21, _toConsumableArray(Beidseitig("21", "vingt-et-un")));
+(_frenchTraining$frage22 = frenchTraining.fragen).push.apply(_frenchTraining$frage22, _toConsumableArray(Beidseitig("22", "vingt-deux")));
+(_frenchTraining$frage23 = frenchTraining.fragen).push.apply(_frenchTraining$frage23, _toConsumableArray(Beidseitig("23", "vingt-trois")));
+(_frenchTraining$frage24 = frenchTraining.fragen).push.apply(_frenchTraining$frage24, _toConsumableArray(Beidseitig("24", "vingt-quatre")));
+(_frenchTraining$frage25 = frenchTraining.fragen).push.apply(_frenchTraining$frage25, _toConsumableArray(Beidseitig("25", "vingt-cinq")));
+(_frenchTraining$frage26 = frenchTraining.fragen).push.apply(_frenchTraining$frage26, _toConsumableArray(Beidseitig("26", "vingt-six")));
+(_frenchTraining$frage27 = frenchTraining.fragen).push.apply(_frenchTraining$frage27, _toConsumableArray(Beidseitig("27", "vingt-sept")));
+(_frenchTraining$frage28 = frenchTraining.fragen).push.apply(_frenchTraining$frage28, _toConsumableArray(Beidseitig("28", "vingt-huit")));
+(_frenchTraining$frage29 = frenchTraining.fragen).push.apply(_frenchTraining$frage29, _toConsumableArray(Beidseitig("29", "vingt-neuf")));
+(_frenchTraining$frage30 = frenchTraining.fragen).push.apply(_frenchTraining$frage30, _toConsumableArray(Beidseitig("30", "trente")));
+
+(_frenchTraining$frage31 = frenchTraining.fragen).push.apply(_frenchTraining$frage31, _toConsumableArray(Beidseitig("der Fr\xFChling", "l'\xE9t\xE9")));
+(_frenchTraining$frage32 = frenchTraining.fragen).push.apply(_frenchTraining$frage32, _toConsumableArray(Beidseitig("der Sommer", "l'\xE9t\xE9")));
+(_frenchTraining$frage33 = frenchTraining.fragen).push.apply(_frenchTraining$frage33, _toConsumableArray(Beidseitig("der Herbst", "l'\xE9t\xE9")));
+(_frenchTraining$frage34 = frenchTraining.fragen).push.apply(_frenchTraining$frage34, _toConsumableArray(Beidseitig("der Winter", "l'\xE9t\xE9")));
+
+(_frenchTraining$frage35 = frenchTraining.fragen).push.apply(_frenchTraining$frage35, _toConsumableArray(Beidseitig("Da bin ich mir sicher!", "J'en suis certain!")));
+(_frenchTraining$frage36 = frenchTraining.fragen).push.apply(_frenchTraining$frage36, _toConsumableArray(Beidseitig("en milieu de journ\xE9e", "Mittags")));
+(_frenchTraining$frage37 = frenchTraining.fragen).push.apply(_frenchTraining$frage37, _toConsumableArray(Beidseitig("plus que tout", "mehr als alles andere")));
+(_frenchTraining$frage38 = frenchTraining.fragen).push.apply(_frenchTraining$frage38, _toConsumableArray(Beidseitig("prochain, prochaine", " n\xE4chster, n\xE4chste")));
+(_frenchTraining$frage39 = frenchTraining.fragen).push.apply(_frenchTraining$frage39, _toConsumableArray(Beidseitig("il a \xE9t\xE9 conspuer", "er wurde ausgebuht")));
+
+(_frenchTraining$frage40 = frenchTraining.fragen).push.apply(_frenchTraining$frage40, _toConsumableArray(Beidseitig("jaune", "gelb")));
+(_frenchTraining$frage41 = frenchTraining.fragen).push.apply(_frenchTraining$frage41, _toConsumableArray(Beidseitig("rouge", "rot")));
+(_frenchTraining$frage42 = frenchTraining.fragen).push.apply(_frenchTraining$frage42, _toConsumableArray(Beidseitig("multicolore", "bunt")));
+(_frenchTraining$frage43 = frenchTraining.fragen).push.apply(_frenchTraining$frage43, _toConsumableArray(Beidseitig("manifestation", "Demonstration")));
+(_frenchTraining$frage44 = frenchTraining.fragen).push.apply(_frenchTraining$frage44, _toConsumableArray(Beidseitig("gilet", "Weste")));
+(_frenchTraining$frage45 = frenchTraining.fragen).push.apply(_frenchTraining$frage45, _toConsumableArray(Beidseitig("plusieur centaines", "mehrere hundert")));
+
+(_frenchTraining$frage46 = frenchTraining.fragen).push.apply(_frenchTraining$frage46, _toConsumableArray(Beidseitig("Montag", "le lundi")));
+(_frenchTraining$frage47 = frenchTraining.fragen).push.apply(_frenchTraining$frage47, _toConsumableArray(Beidseitig("Dienstag", "le mardi")));
+(_frenchTraining$frage48 = frenchTraining.fragen).push.apply(_frenchTraining$frage48, _toConsumableArray(Beidseitig("Mittwoch", "le mercredi")));
+(_frenchTraining$frage49 = frenchTraining.fragen).push.apply(_frenchTraining$frage49, _toConsumableArray(Beidseitig("Donnerstag", "le jeudi")));
+(_frenchTraining$frage50 = frenchTraining.fragen).push.apply(_frenchTraining$frage50, _toConsumableArray(Beidseitig("Freitag", "le vendredi")));
+(_frenchTraining$frage51 = frenchTraining.fragen).push.apply(_frenchTraining$frage51, _toConsumableArray(Beidseitig("Samstag", "le samedi")));
+(_frenchTraining$frage52 = frenchTraining.fragen).push.apply(_frenchTraining$frage52, _toConsumableArray(Beidseitig("Sonntag", "le dimanche")));
 
 trainings.push(frenchTraining);
--- js6/francais.js
+++ js6/francais.js
@@ -10,11 +10,58 @@ frenchTraining.fragen.push(... Beidseitig(`2`, `deux`));
 frenchTraining.fragen.push(... Beidseitig(`3`, `trois`));
 frenchTraining.fragen.push(... Beidseitig(`4`, `quatre`));
 frenchTraining.fragen.push(... Beidseitig(`5`, `cinq`));
-
 frenchTraining.fragen.push(... Beidseitig(`6`, `six`));
 frenchTraining.fragen.push(... Beidseitig(`7`, `sept`));
 frenchTraining.fragen.push(... Beidseitig(`8`, `huit`));
 frenchTraining.fragen.push(... Beidseitig(`9`, `neuf`));
 frenchTraining.fragen.push(... Beidseitig(`10`, `dix`));
+
+frenchTraining.fragen.push(... Beidseitig(`11`, `onze`));
+frenchTraining.fragen.push(... Beidseitig(`12`, `douze`));
+frenchTraining.fragen.push(... Beidseitig(`13`, `treize`));
+frenchTraining.fragen.push(... Beidseitig(`14`, `quatorze`));
+frenchTraining.fragen.push(... Beidseitig(`15`, `quinze`));
+frenchTraining.fragen.push(... Beidseitig(`16`, `seize`));
+frenchTraining.fragen.push(... Beidseitig(`17`, `dix-sept`));
+frenchTraining.fragen.push(... Beidseitig(`18`, `dix-huit`));
+frenchTraining.fragen.push(... Beidseitig(`19`, `dix-neuf`));
+frenchTraining.fragen.push(... Beidseitig(`20`, `vingt`));
+
+frenchTraining.fragen.push(... Beidseitig(`21`, `vingt-et-un`));
+frenchTraining.fragen.push(... Beidseitig(`22`, `vingt-deux`));
+frenchTraining.fragen.push(... Beidseitig(`23`, `vingt-trois`));
+frenchTraining.fragen.push(... Beidseitig(`24`, `vingt-quatre`));
+frenchTraining.fragen.push(... Beidseitig(`25`, `vingt-cinq`));
+frenchTraining.fragen.push(... Beidseitig(`26`, `vingt-six`));
+frenchTraining.fragen.push(... Beidseitig(`27`, `vingt-sept`));
+frenchTraining.fragen.push(... Beidseitig(`28`, `vingt-huit`));
+frenchTraining.fragen.push(... Beidseitig(`29`, `vingt-neuf`));
+frenchTraining.fragen.push(... Beidseitig(`30`, `trente`));
+
+frenchTraining.fragen.push(... Beidseitig(`der Frühling`, `l'été`));
+frenchTraining.fragen.push(... Beidseitig(`der Sommer`, `l'été`));
+frenchTraining.fragen.push(... Beidseitig(`der Herbst`, `l'été`));
+frenchTraining.fragen.push(... Beidseitig(`der Winter`, `l'été`));
+
+frenchTraining.fragen.push(... Beidseitig(`Da bin ich mir sicher!`, `J'en suis certain!`));
+frenchTraining.fragen.push(... Beidseitig(`en milieu de journée`, `Mittags`));
+frenchTraining.fragen.push(... Beidseitig(`plus que tout`, `mehr als alles andere`));
+frenchTraining.fragen.push(... Beidseitig(`prochain, prochaine`, ` nächster, nächste`));
+frenchTraining.fragen.push(... Beidseitig(`il a été conspuer`, `er wurde ausgebuht`));
+
+frenchTraining.fragen.push(... Beidseitig(`jaune`, `gelb`));
+frenchTraining.fragen.push(... Beidseitig(`rouge`, `rot`));
+frenchTraining.fragen.push(... Beidseitig(`multicolore`, `bunt`));
+frenchTraining.fragen.push(... Beidseitig(`manifestation`, `Demonstration`));
+frenchTraining.fragen.push(... Beidseitig(`gilet`, `Weste`));
+frenchTraining.fragen.push(... Beidseitig(`plusieur centaines`, `mehrere hundert`));
+
+frenchTraining.fragen.push(... Beidseitig(`Montag`, `le lundi`));
+frenchTraining.fragen.push(... Beidseitig(`Dienstag`, `le mardi`));
+frenchTraining.fragen.push(... Beidseitig(`Mittwoch`, `le mercredi`));
+frenchTraining.fragen.push(... Beidseitig(`Donnerstag`, `le jeudi`));
+frenchTraining.fragen.push(... Beidseitig(`Freitag`, `le vendredi`));
+frenchTraining.fragen.push(... Beidseitig(`Samstag`, `le samedi`));
+frenchTraining.fragen.push(... Beidseitig(`Sonntag`, `le dimanche`)); 
 
 trainings.push(frenchTraining);
