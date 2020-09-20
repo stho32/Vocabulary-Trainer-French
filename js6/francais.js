cecfd6787997d31873ab50f4468571f872f3760c
Stefan Hoffmann <stho32github@gmail.com>
2020-09-20 10:23:40 +0200

    updated vocabulary from web app

--- js6/francais.js
+++ js6/francais.js
@@ -1,3 +1,4 @@
+
 var frenchTraining = {
     name: "Apprendre français",
     description: "Learning french with Fawda",
@@ -5,63 +6,89 @@ var frenchTraining = {
     fragen: []
 };
 
-frenchTraining.fragen.push(... Beidseitig(`1`, `un`));
-frenchTraining.fragen.push(... Beidseitig(`2`, `deux`));
-frenchTraining.fragen.push(... Beidseitig(`3`, `trois`));
-frenchTraining.fragen.push(... Beidseitig(`4`, `quatre`));
-frenchTraining.fragen.push(... Beidseitig(`5`, `cinq`));
-frenchTraining.fragen.push(... Beidseitig(`6`, `six`));
-frenchTraining.fragen.push(... Beidseitig(`7`, `sept`));
-frenchTraining.fragen.push(... Beidseitig(`8`, `huit`));
-frenchTraining.fragen.push(... Beidseitig(`9`, `neuf`));
-frenchTraining.fragen.push(... Beidseitig(`10`, `dix`));
-
-frenchTraining.fragen.push(... Beidseitig(`11`, `onze`));
-frenchTraining.fragen.push(... Beidseitig(`12`, `douze`));
-frenchTraining.fragen.push(... Beidseitig(`13`, `treize`));
-frenchTraining.fragen.push(... Beidseitig(`14`, `quatorze`));
-frenchTraining.fragen.push(... Beidseitig(`15`, `quinze`));
-frenchTraining.fragen.push(... Beidseitig(`16`, `seize`));
-frenchTraining.fragen.push(... Beidseitig(`17`, `dix-sept`));
-frenchTraining.fragen.push(... Beidseitig(`18`, `dix-huit`));
-frenchTraining.fragen.push(... Beidseitig(`19`, `dix-neuf`));
-frenchTraining.fragen.push(... Beidseitig(`20`, `vingt`));
-
-frenchTraining.fragen.push(... Beidseitig(`21`, `vingt-et-un`));
-frenchTraining.fragen.push(... Beidseitig(`22`, `vingt-deux`));
-frenchTraining.fragen.push(... Beidseitig(`23`, `vingt-trois`));
-frenchTraining.fragen.push(... Beidseitig(`24`, `vingt-quatre`));
-frenchTraining.fragen.push(... Beidseitig(`25`, `vingt-cinq`));
-frenchTraining.fragen.push(... Beidseitig(`26`, `vingt-six`));
-frenchTraining.fragen.push(... Beidseitig(`27`, `vingt-sept`));
-frenchTraining.fragen.push(... Beidseitig(`28`, `vingt-huit`));
-frenchTraining.fragen.push(... Beidseitig(`29`, `vingt-neuf`));
-frenchTraining.fragen.push(... Beidseitig(`30`, `trente`));
-
+frenchTraining.fragen.push(... Beidseitig(`eins`, `un`));
+frenchTraining.fragen.push(... Beidseitig(`zwei`, `deux`));
+frenchTraining.fragen.push(... Beidseitig(`drei`, `trois`));
+frenchTraining.fragen.push(... Beidseitig(`vier`, `quatre`));
+frenchTraining.fragen.push(... Beidseitig(`fünf`, `cinq`));
+frenchTraining.fragen.push(... Beidseitig(`sechs`, `six`));
+frenchTraining.fragen.push(... Beidseitig(`sieben`, `sept`));
+frenchTraining.fragen.push(... Beidseitig(`acht`, `neuf`));
+frenchTraining.fragen.push(... Beidseitig(`zehn`, `dix`));
+frenchTraining.fragen.push(... Beidseitig(`elf`, `onze`));
+frenchTraining.fragen.push(... Beidseitig(`zwölf`, `douze`));
+frenchTraining.fragen.push(... Beidseitig(`dreizehn`, `treize`));
+frenchTraining.fragen.push(... Beidseitig(`vierzehn`, `quatorze`));
+frenchTraining.fragen.push(... Beidseitig(`fünfzehn`, `quinze`));
+frenchTraining.fragen.push(... Beidseitig(`sechzehn`, `seize`));
+frenchTraining.fragen.push(... Beidseitig(`siebzehn`, `dix-sept`));
+frenchTraining.fragen.push(... Beidseitig(`achtzehn`, `dix-huit`));
+frenchTraining.fragen.push(... Beidseitig(`neunzehn`, `dix-neuf`));
+frenchTraining.fragen.push(... Beidseitig(`zwanzig`, `vingt`));
+frenchTraining.fragen.push(... Beidseitig(`einundzwanzig`, `vingt-et-un`));
+frenchTraining.fragen.push(... Beidseitig(`zweiundzwanzig`, `vingt-deux`));
+frenchTraining.fragen.push(... Beidseitig(`dreiundzwanzig`, `vingt-trois`));
+frenchTraining.fragen.push(... Beidseitig(`vierundzwanzig`, `vingt-quatre`));
+frenchTraining.fragen.push(... Beidseitig(`fünfundzwanzig`, `vingt-cinq`));
+frenchTraining.fragen.push(... Beidseitig(`sechsundzwanzig`, `vingt-six`));
+frenchTraining.fragen.push(... Beidseitig(`siebenundzwanzig`, `vingt-sept`));
+frenchTraining.fragen.push(... Beidseitig(`achtundzwanzig`, `vingt-huit`));
+frenchTraining.fragen.push(... Beidseitig(`neunundzwanzig`, `vingt-neuf`));
+frenchTraining.fragen.push(... Beidseitig(`dreißig`, `trente`));
 frenchTraining.fragen.push(... Beidseitig(`der Frühling`, `le printemps`));
 frenchTraining.fragen.push(... Beidseitig(`der Sommer`, `l'été`));
 frenchTraining.fragen.push(... Beidseitig(`der Herbst`, `l'automne`));
 frenchTraining.fragen.push(... Beidseitig(`der Winter`, `l'hiver`));
-
 frenchTraining.fragen.push(... Beidseitig(`Da bin ich mir sicher!`, `J'en suis certain!`));
-frenchTraining.fragen.push(... Beidseitig(`en milieu de journée`, `Mittags`));
-frenchTraining.fragen.push(... Beidseitig(`plus que tout`, `mehr als alles andere`));
-frenchTraining.fragen.push(... Beidseitig(`prochain, prochaine`, ` nächster, nächste`));
-frenchTraining.fragen.push(... Beidseitig(`il a été conspuer`, `er wurde ausgebuht`));
+frenchTraining.fragen.push(... Beidseitig(`mittags`, `en milieu de journée`));
+frenchTraining.fragen.push(... Beidseitig(`mehr als alles andere`, `plus que tout`));
+frenchTraining.fragen.push(... Beidseitig(`nächste, nächster`, `prochain, prochaine`));
+frenchTraining.fragen.push(... Beidseitig(`er wurde ausgebuht`, `il a été conspuer`));
+frenchTraining.fragen.push(... Beidseitig(`gelb`, `jaune`));
+frenchTraining.fragen.push(... Beidseitig(`rot`, `rouge`));
+frenchTraining.fragen.push(... Beidseitig(`bunt`, `multicolore`));
+frenchTraining.fragen.push(... Beidseitig(`Demonstration`, `manifestation`));
+frenchTraining.fragen.push(... Beidseitig(`Weste`, `gilet`));
+frenchTraining.fragen.push(... Beidseitig(`mehrere hundert`, `plusieur centaines`));
+frenchTraining.fragen.push(... Beidseitig(`der Montag`, `le lundi`));
+frenchTraining.fragen.push(... Beidseitig(`der Dienstag`, `le mardi`));
+frenchTraining.fragen.push(... Beidseitig(`der Mittwoch`, `le mecredi`));
+frenchTraining.fragen.push(... Beidseitig(`der Donnerstag`, `le jeudi`));
+frenchTraining.fragen.push(... Beidseitig(`der Freitag`, `le vendredi`));
+frenchTraining.fragen.push(... Beidseitig(`der Samstag`, `le samdi`));
+frenchTraining.fragen.push(... Beidseitig(`der Sonntag`, `le dimanche`));
+frenchTraining.fragen.push(... Beidseitig(`Hattest Du eine schöne Woche?`, `As-tu passé une belle semaine?`));
+frenchTraining.fragen.push(... Beidseitig(`gestern`, `hier`));
+frenchTraining.fragen.push(... Beidseitig(`morgen`, `demain`));
+frenchTraining.fragen.push(... Beidseitig(`heute`, `aujourd'hui`));
+frenchTraining.fragen.push(... Beidseitig(`Er ist bekannt.`, `Il est connu.`));
+frenchTraining.fragen.push(... Beidseitig(`Ich habe ihn im Fernsehen gesehen.`, `Je l'ai vu à la télevision.`));
+frenchTraining.fragen.push(... Beidseitig(`weniger`, `moins`));
+frenchTraining.fragen.push(... Beidseitig(`Und wieder etwas, dass ich nicht wusste!`, `Encore, quelque chose que je ne savai pas!`));
+frenchTraining.fragen.push(... Beidseitig(`Ich wusste das nicht.`, `Je ne le savai pas.`));
+frenchTraining.fragen.push(... Beidseitig(`Du wusstest das nicht.`, `Tu ne le savais pas.`));
+frenchTraining.fragen.push(... Beidseitig(`Er wusste das nicht.`, `Il ne le savait pas.`));
+frenchTraining.fragen.push(... Beidseitig(`Sie wusste das nicht.`, `Elle ne le savait pas.`));
+frenchTraining.fragen.push(... Beidseitig(`Wir wussten das nicht.`, `Nous ne le savions pas.`));
+frenchTraining.fragen.push(... Beidseitig(`Sie wussten das nicht.`, `Vous ne le saviez pas.`));
+frenchTraining.fragen.push(... Beidseitig(`Wir haben weniger Covid-19-Fälle.`, `Nous avons moins de cas de Covid-19.`));
+frenchTraining.fragen.push(... Beidseitig(`Du machst einen großartigen Job!`, `Vous faites un excellent travail!`));
+frenchTraining.fragen.push(... Beidseitig(`der Professor für Medizin`, `le professeur de médecine`));
+frenchTraining.fragen.push(... Beidseitig(`dem in Deutschland am meisten zugehört wird`, `le plus écouté d’Allemagne`));
+frenchTraining.fragen.push(... Beidseitig(`Bleib gesund!`, `Reste en bonne santé!`));
+frenchTraining.fragen.push(... Beidseitig(`letzter, letzte`, `dernier, derniére`));
+frenchTraining.fragen.push(... Beidseitig(`die Bilanz`, `le bilan`));
+frenchTraining.fragen.push(... Beidseitig(`das Robert Koch Intitut`, `l’Institut de santé Robert Koch`));
+frenchTraining.fragen.push(... Beidseitig(`M. Drosten hat einige Erklärungen`, `M. Drosten avance quelques explications`));
+frenchTraining.fragen.push(... Beidseitig(`Es gibt einige kleine Dinge, die uns von Südeuropa unterscheiden.`, `Il y a quelques petites choses qui nous distinguent de l’Europe du Sud.`));
+frenchTraining.fragen.push(... Beidseitig(`Unsere Haushalte sind kleiner.`, `Nos foyers sont souvent moins nombreux.`));
+frenchTraining.fragen.push(... Beidseitig(`Dies sind sicherlich kleine Besonderheiten.`, `Ce sont à coup sûr des petites singularités.`));
+frenchTraining.fragen.push(... Beidseitig(`Nachbarn`, `voisins`));
+frenchTraining.fragen.push(... Beidseitig(`Deshalb müssen wir sehr vorsichtig bleiben.`, `C’est pourquoi nous devons continuer à être très prudents.`));
+frenchTraining.fragen.push(... Beidseitig(`hinter dem Rhein`, `outre-Rhin`));
+frenchTraining.fragen.push(... Beidseitig(`Keine Ursache!`, `Il n'y a pas de quoi!`));
+frenchTraining.fragen.push(... Beidseitig(`Wir brauchen diese drei Arten von Anwendungen.`, `Nous avons besoin de ces trois types d'application.`));
 
-frenchTraining.fragen.push(... Beidseitig(`jaune`, `gelb`));
-frenchTraining.fragen.push(... Beidseitig(`rouge`, `rot`));
-frenchTraining.fragen.push(... Beidseitig(`multicolore`, `bunt`));
-frenchTraining.fragen.push(... Beidseitig(`manifestation`, `Demonstration`));
-frenchTraining.fragen.push(... Beidseitig(`gilet`, `Weste`));
-frenchTraining.fragen.push(... Beidseitig(`plusieur centaines`, `mehrere hundert`));
-
-frenchTraining.fragen.push(... Beidseitig(`Montag`, `le lundi`));
-frenchTraining.fragen.push(... Beidseitig(`Dienstag`, `le mardi`));
-frenchTraining.fragen.push(... Beidseitig(`Mittwoch`, `le mercredi`));
-frenchTraining.fragen.push(... Beidseitig(`Donnerstag`, `le jeudi`));
-frenchTraining.fragen.push(... Beidseitig(`Freitag`, `le vendredi`));
-frenchTraining.fragen.push(... Beidseitig(`Samstag`, `le samedi`));
-frenchTraining.fragen.push(... Beidseitig(`Sonntag`, `le dimanche`)); 
 
 trainings.push(frenchTraining);
+
